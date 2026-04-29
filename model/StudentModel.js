3fb7852a82de432fae6d9bb1e18a7c3110443bf6
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-04-29 17:24:35 +0530

    created student model

--- model/StudentModel.js
+++ model/StudentModel.js
@@ -1,3 +1,5 @@
+import {student_db_array} from "../db/db.js";
+
 class Student{
     #id;
     #name;
@@ -47,8 +49,48 @@ class Student{
 
 // ===================== Add Student ===========================
 
+const addToStudents = (id, name, nic, phone, address) => {
+    let student = new Student(id, name, nic, phone, address);
+
+    student_db_array.push(student);
+}
 // ===================== Update Student ===========================
+
+const updateStudentData = (id,name, nic, phone, address) => {
+    let obj = student_db_array.find(item => item.id == id);
+
+    if(obj){
+        obj.id = id;
+        obj.name = name;
+        obj.nic = nic;
+        obj.phone = phone;
+        obj.address = address;
+    }
+}
 
 // ===================== Delete Student ===========================
 
-// ===================== Load Student Data ===========================
+const deleteStudent = (id) => {
+    let index = student_db_array.findIndex(item => item.id == id);
+    student_db_array.splice(index, 1);
+    addToStudentTable();
+}
+
+// ===================== Get Student ===========================
+
+const getStudentData = () => {
+    return student_db_array;
+}
+// ===================== Get Student By Index ===========================
+
+const getStudentDataByIndex = (index) => {
+    return student_db_array[index];
+}
+
+// ===================== Get Student By Id ===========================
+
+const getStudentDataById = (id) => {
+    return student_db_array.find(item => item.id == id);
+}
+
+export {addToStudents,updateStudentData,deleteStudent,getStudentDataById,getStudentData,getStudentDataByIndex};
